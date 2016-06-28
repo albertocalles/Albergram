129e097f9e18ba91b85b6c7913531937fab50c6e
Alberto Calles <torresvk@gmail.com>
2016-06-28 20:01:18 +0100

    Agregando JavaScript en el cliente

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var numeros = [400,200,1,-23];
+
+//var numerosMas1 = numeros.map(function(numero){
+//	return numero + 1;
+//});
+
+var numerosMas1 = numeros.map(v => v + 1));
+
+console.log(numerosMas1);
