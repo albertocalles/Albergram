f25499b047042a130ea61854b8fa7369e5f82c50
Alberto Calles <torresvk@gmail.com>
2016-06-29 19:57:22 +0100

    Modularizando nuestro proyecto

---
+++ src/homepage/index.js
@@ -0,0 +1,10 @@
+var page = require('page');
+var empty = require('empty-element');
+var template = require('./template');
+
+page('/', function(ctx, next){
+	
+	var main = document.getElementById('main-container');
+	main.innerHTML = '<a href="/signup">Signup</a>';
+	
+});
