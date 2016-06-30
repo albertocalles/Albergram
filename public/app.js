c1eb66a282d7286bdd6f758ba9af0e0312d2381d
Alberto Calles <torresvk@gmail.com>
2016-06-30 06:46:23 +0100

    Agregando la página de /signin

--- public/app.js
+++ public/app.js
@@ -2239,59 +2239,129 @@ module.exports = [
 
 },{}],14:[function(require,module,exports){
 var page = require('page');
-var yo = require('yo-yo');
 var empty = require('empty-element');
-
-var main = document.getElementById('main-container');
+var template = require('./template');
 
 page('/', function (ctx, next) {
+
+	var main = document.getElementById('main-container');
 	main.innerHTML = '<a href="/signup">Signup</a>';
 });
 
-page('/signup', function (ctx, next) {
+},{"./template":15,"empty-element":3,"page":9}],15:[function(require,module,exports){
 
-	var el = yo`<div class="container">
-			<div class="row">
-				<div class="col s10 push-s1">
-					<div class="row">
-						<div class="col m5 hide-on-small-only">
-							<img class="iphone" src="iphone.png" />
-						</div>
-						<div class="col s12 m7">
-							<div class="row">
-								<div class="signup-box">
-									<h1 class="albergram">Albergram</h1>
-									<form class="signup-form">
-										<h2>Registrate para ver fotos de tus amigos en Albergram</h2>
-										<div class="section">
-											<a class="btn btn-fb hide-on-small-only">Iniciar sesi�n con Facebook</a>
-											<a class="btn btn-fb hide-on-med-and-up">Iniciar sesi�n</a>
-										</div>
-										<div class="divider"></div>
-										<div class="section">
-											<input type="email" name="email" placeholder="Correo electr�nico">
-											<input type="text" name="name" placeholder="Nombre completo">
-											<input type="text" name="username" placeholder="Nombre de usuario">
-											<input type="password" name="password" placeholder="Contrase�a">
-											<button class="btn waves-effect waves-light btn-signup" type="submit">Reg�strate</button>
-										</div>
-									</form>
-								</div>
-							</div>
-							<div class="row">
-								<div class="login-box">
-								�Tienes una cuenta? <a href="/signin">Entrar</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>`;
+},{}],16:[function(require,module,exports){
+var page = require('page');
 
-	empty(main).appendChild(el);
-});
+require('./homepage');
+require('./signup');
+require('./signin');
 
 page();
 
-},{"empty-element":3,"page":9,"yo-yo":12}]},{},[14]);
+},{"./homepage":14,"./signin":18,"./signup":20,"page":9}],17:[function(require,module,exports){
+var yo = require('yo-yo');
+
+module.exports = function landing(box) {
+
+	return yo`<div class="container">
+		<div class="row">
+			<div class="col s10 push-s1">
+				<div class="row">
+					<div class="col m5 hide-on-small-only">
+						<img class="iphone" src="iphone.png" />
+					</div>
+					${ box }
+				</div>
+			</div>
+		</div>
+	</div>`;
+};
+
+},{"yo-yo":12}],18:[function(require,module,exports){
+var page = require('page');
+var empty = require('empty-element');
+var template = require('./template');
+
+page('/signin', function (ctx, next) {
+	var main = document.getElementById('main-container');
+	empty(main).appendChild(template);
+});
+
+},{"./template":19,"empty-element":3,"page":9}],19:[function(require,module,exports){
+var yo = require('yo-yo');
+var landing = require('../landing');
+
+var signinForm = yo`<div class="col s12 m7">
+	<div class="row">
+		<div class="signup-box">
+			<h1 class="albergram">Albergram</h1>
+			<form class="signup-form">
+				<div class="section">
+					<a class="btn btn-fb hide-on-small-only">Iniciar sesi�n con Facebook</a>
+					<a class="btn btn-fb hide-on-med-and-up">Iniciar sesi�n</a>
+				</div>
+				<div class="divider"></div>
+				<div class="section">
+					<input type="text" name="username" placeholder="Nombre de usuario">
+					<input type="password" name="password" placeholder="Contrase�a">
+					<button class="btn waves-effect waves-light btn-signup" type="submit">Iniciar sesi�n</button>
+				</div>
+			</form>
+		</div>
+	</div>
+	<div class="row">
+		<div class="login-box">
+		�No tienes una cuenta? <a href="/signup">Reg�strate</a>
+		</div>
+	</div>
+</div>`;
+
+module.exports = landing(signinForm);
+
+},{"../landing":17,"yo-yo":12}],20:[function(require,module,exports){
+var page = require('page');
+var empty = require('empty-element');
+var template = require('./template');
+
+page('/signup', function (ctx, next) {
+
+	var main = document.getElementById('main-container');
+	empty(main).appendChild(template);
+});
+
+},{"./template":21,"empty-element":3,"page":9}],21:[function(require,module,exports){
+var yo = require('yo-yo');
+var landing = require('../landing');
+
+var signupForm = yo`<div class="col s12 m7">
+	<div class="row">
+		<div class="signup-box">
+			<h1 class="albergram">Albergram</h1>
+			<form class="signup-form">
+				<h2>Registrate para ver fotos de tus amigos en Albergram</h2>
+				<div class="section">
+					<a class="btn btn-fb hide-on-small-only">Iniciar sesi�n con Facebook</a>
+					<a class="btn btn-fb hide-on-med-and-up">Iniciar sesi�n</a>
+				</div>
+				<div class="divider"></div>
+				<div class="section">
+					<input type="email" name="email" placeholder="Correo electr�nico">
+					<input type="text" name="name" placeholder="Nombre completo">
+					<input type="text" name="username" placeholder="Nombre de usuario">
+					<input type="password" name="password" placeholder="Contrase�a">
+					<button class="btn waves-effect waves-light btn-signup" type="submit">Reg�strate</button>
+				</div>
+			</form>
+		</div>
+	</div>
+	<div class="row">
+		<div class="login-box">
+		�Tienes una cuenta? <a href="/signin">Entrar</a>
+		</div>
+	</div>
+</div>`;
+
+module.exports = landing(signupForm);
+
+},{"../landing":17,"yo-yo":12}]},{},[16]);
